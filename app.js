74fd558b32b138609472c74dfd0bdb059e94eac9
Aditya <aditya.031122@gmail.com>
2025-04-15 02:06:22 +0530

    error-handling

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderLust";
 main().then(()=>{
@@ -27,10 +29,10 @@ app.get("/",(req,res)=>{
 });
 
 //index route
-app.get("/listings", async (req, res) => {
+app.get("/listings",wrapAsync(async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index.ejs", { allListings });
-});
+}));
 
 //new route
 app.get("/listings/new",(req,res)=>{
@@ -38,48 +40,52 @@ app.get("/listings/new",(req,res)=>{
 });
 
 //show route
-app.get("/listings/:id", async (req, res) => {
-  const { id } = req.params; 
-  try {
-    const listing = await Listing.findById(id);
-    if (!listing) {
-      return res.status(404).send("Listing not found");
-    }
-    res.render("listings/show.ejs", { listing });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Internal Server Error");
+app.get("/listings/:id", wrapAsync(async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  if (!listing) {
+    throw new ExpressError(404, "Listing not found");
   }
-});
+  res.render("listings/show.ejs", { listing });
+}));
 
 //create route
-app.post("/listings",async(req, res)=>{
+app.post(
+  "/listings",
+  wrapAsync (async(req, res, next)=>{
+    if(!req.body.listing){
+      throw new ExpressError(400, "Send valid data for listing");
+    }
   const newListing =  new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
-});
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
 
 //Edit Route
-app.get("/listings/:id/edit",async (req, res)=>{
+app.get("/listings/:id/edit",wrapAsync(async (req, res)=>{
   let{id}=req.params;
   const listing = await Listing.findById(id);
   res.render("listings/edit.ejs", {listing});
-});
+}));
 
 //update route
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", wrapAsync(async (req, res) => {
+  if(!req.body.listing){
+    throw new ExpressError(400, "Send valid data for listing");
+  }
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect(`/listings/${id}`);
-});
+}));
 
 //Delete Route
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
   let { id } = req.params;
   let deletedListing = await Listing.findByIdAndDelete(id);
   console.log(deletedListing);
   res.redirect("/listings");
-});
+}));
 
 
 // app.get("/testListening",async(req, res)=>{
@@ -94,6 +100,21 @@ app.delete("/listings/:id", async (req, res) => {
 //   console.log("sample was saved");
 //   res.send("successful testing");
 // });
+
+app.all("*", (req, res, next)=>{
+  next(new ExpressError(404, "Page Not Found!"));
+});
+
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went wrong!" } = err;
+
+  if (err.name === "CastError") {
+    statusCode = 400;
+    message = `Invalid ID format: ${err.value}`;
+  }
+
+  res.status(statusCode).render("error.ejs", { message });
+});
 
 app.listen(8080, () =>{
   console.log("Server is listening to port 8080");
